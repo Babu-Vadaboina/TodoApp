9b1005949fc260acd87dcc59bf85d8a24bd276a2
Babu-Vadaboina <tatababuvadaboina@gmail.com>
2025-04-29 22:37:06 +0530

    added skeleton for frontend

---
+++ src/Home.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import Input from "./Input";
+
+export default function Home() {
+  const [todos, setTodos] = useState([]);
+  return (
+    <div>
+      <h2>Todo List</h2>
+      <Input />
+      {todos.length === 0 ? (
+        <h2>No Todos</h2>
+      ) : (
+        todos.map((todo) => {
+          todo;
+        })
+      )}
+    </div>
+  );
+}
---
+++ src/Input.jsx
@@ -0,0 +1,10 @@
+export default function Input() {
+  return (
+    <div>
+      <div>
+        <input type="text" id="" name="" />
+        <button type="button">Add</button>
+      </div>
+    </div>
+  );
+}
