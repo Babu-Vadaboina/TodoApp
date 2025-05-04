8e41807fb4049cfe27ee46f61f109f21a5083870
Babu-Vadaboina <tatababuvadaboina@gmail.com>
2025-05-04 13:10:50 +0530

    added the basic skeleton for the fronend successfully

--- Frontend/src/Input.jsx
+++ Frontend/src/Input.jsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
+import axios from "axios";
+
 export default function Input() {
+  const [task, setTask] = useState();
+  const handleAdd = () => {
+    axios
+      .post("http://localhost:3000/add", { task: task })
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => console.log(err));
+  };
   return (
     <div>
       <div className="text-center">
-        <input type="text" id="" name="" />
-        <button type="button">Add</button>
+        <input type="text" onChange={(e) => setTask(e.target.value)} />
+        <button type="button" onClick={handleAdd}>
+          Add
+        </button>
       </div>
     </div>
   );
